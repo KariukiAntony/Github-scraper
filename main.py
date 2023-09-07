d28e4f95e0f6ddfebc84a4b5d3820e7d1be4178e
KariukiAntony <antonygichoya9@gmail.com>
2023-09-07 15:57:40 +0300

    specify the port of my server

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from api import create_app
 app = create_app()
 
 if __name__ == "__main__":
-          uvicorn.run("main:app", reload=True)
+          uvicorn.run("main:app", reload=True, port=5000)
