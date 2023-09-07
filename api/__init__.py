48b6636b5b872090763ff5a2093effc9b5cb0f2c
KariukiAntony <antonygichoya9@gmail.com>
2023-09-07 15:55:22 +0300

    create server and test it

--- api/__init__.py
+++ api/__init__.py
@@ -3,5 +3,8 @@ from fastapi import FastAPI
 def create_app():
 
           app = FastAPI()
+          @app.get("/")
+          def index():
+                  return {"message": "Hello world"}
 
           return app
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import uvicorn
+from api import create_app
 
+app = create_app()
 
+if __name__ == "__main__":
+          uvicorn.run("main:app", reload=True)
