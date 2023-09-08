e173323bf18d9110cdfb6968153059d7285dd71c
KariukiAntony <antonygichoya9@gmail.com>
2023-09-08 12:39:31 +0300

    get user avarter image

--- api/github.py
+++ api/github.py
@@ -16,8 +16,9 @@ def get_user_credentials(user: User):
           username = user.username
           content = check_if_user_has_an_account(username)
           if content:
-                  print("Yes, has a github username")
-                  return {"success":f"user with username: {username} have a github account" }, status.HTTP_200_OK
+                  content_info = BeautifulSoup(content, "html.parser")
+                  avarter_image = get_avarter_image(content=content_info)
+                  return {"avarter_image":avarter_image }, status.HTTP_200_OK
                   
 
           return {"failed":f"user with username: {username} does not have a github account" }, status.HTTP_404_NOT_FOUND
@@ -30,3 +31,11 @@ def check_if_user_has_an_account(username: str):
                 return response.content
         
         return False
+
+def get_avarter_image(content):
+        avarter_image = content.find("div", class_="js-profile-editable-replace")
+        avarter_image = avarter_image.find("a")
+        src = avarter_image.find("img").get("src")
+        alt = avarter_image.find("img").get("alt")
+        print({"src": src, "alt": alt} )
+        return {"src": src, "alt": alt}
